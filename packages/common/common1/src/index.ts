99cd5df9df5177179d79dae31f41096b97fd0e5d
t-akasaki <t-akasaki@systemzeus.co.jp>
2023-07-04 17:29:57 +0900

    package.jsonでライブラリの指定を変更

--- packages/common/common1/src/index.ts
+++ packages/common/common1/src/index.ts
@@ -1,6 +1,13 @@
 export function add(a: number, b: number) {
   return a + b
 }
+
+/**
+ * 
+ * @param a 
+ * @param b 
+ * @returns 
+ */
 export function diff(a: number, b: number) {
   return a - b
 }
